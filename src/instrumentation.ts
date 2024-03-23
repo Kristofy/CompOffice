2a586c4928df33936ae118a6f33163917523b67a
Osztopáni Kristóf <kristofosztopani@gmail.com>
2024-03-23 19:20:57 +0100

    Added new ENV vars

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,6 @@
 
 export async function register() {
-    const { validateEnv } = await import("@/server/validate-env")
+    const { validateEnv } = await import("@/server/env-schema")
     validateEnv()
     const { ENV } = await import("@/server/env")
     console.log(`Running in ${ENV.PRODUCTION ? "PROD" : "DEV" } mode`) 
