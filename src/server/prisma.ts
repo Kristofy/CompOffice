3f7b96b3d7096b0d4d6917fa0adb7a9d2a24ab83
Osztopáni Kristóf <kristofosztopani@gmail.com>
2024-03-23 19:22:20 +0100

    Bugfix: reload client was reloaded by dev-hotreload

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,14 +1,15 @@
 import { ENV } from './env';
 import { PrismaClient } from '@prisma/client';
 
-const prismaGlobal = global as typeof global & {
-  prisma?: PrismaClient;
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 // TODO(Kristofy): Look into redis caching (https://www.npmjs.com/package/prisma-redis-middleware)
-export const prisma: PrismaClient =
-  prismaGlobal.prisma ??
+export const prisma: PrismaClient = globalThis.prisma ||
   new PrismaClient({
     log:
       ENV.PRODUCTION ? ['error'] : ['query', 'error', 'warn'] ,
   });
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
