481b424b84a05bcfbf310c256c0bdfddbe24ec56
Osztopáni Kristóf <kristofosztopani@gmail.com>
2024-03-23 19:02:55 +0100

    Added dev tools configuration

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	semi: true,
+	trailingComma: 'es5',
+	tabWidth: 2,
+	printWidth: 100,
+	bracketSameLine: true,
+	tailwindConfig: './tailwind.config.ts',
+};
